03cb8bc03db125506dfb4cc3639527330a31fed4
goofyahead <goofyahead@goofyahead-VirtualBox.(none)>
2013-02-05 17:59:16 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var HTTP_PORT= 8080;
+var HTTPS_PORT = 443;
+var LAPSE = 600000;
+
+var API_KEY = "AIzaSyDiNhFxatn5SN25Mb1CqAN0lhkHw9do5l4"
+
+var videosArray = [
+	{name: 'ChessLive', id: '6SRwJVXxMzU'},
+	{name: 'CompanyHorse', id: '_iaP-f65iuE'},
+	{name: 'Dafply', id: '5HFbLfVLNxg'},
+	{name: 'EasyPark', id: 'DHtxQWAG_iI'},
+	{name: 'GigWorking', id: 'TzTphy93KHQ'},
+	{name: 'Hemav', id: 'J5GlNX4E3Kc'},
+	{name: 'Newin', id: 'tQb0s154ZYQ'},
+	{name: 'Noctambuls', id: 'ArJXK8ZySxo'},
+	{name: 'Onfan', id: 'i9upp1EKJuk'},
+	{name: 'Phobius', id: 'k8N0dwa4kS4'},
+	{name: 'Qmunify', id: 'oDA-SkumLfQ'},
+	{name: 'ZeedProtection', id: 'H4cVEQAw7QE'},
+	{name: 'ManagEat', id: 'tHmSK8-ykjY' },
+	{name: 'Exivit', id: 'pKqvoIfLRPo'}
+];
+
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname + '/data'));
+});
+
+app.listen(HTTP_PORT);
+console.log('Listening on port 8080...');
+
+app.get('/', function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+  	res.write("Datos recopilados");
+  	res.write('<ul>');
+  	videosArray.forEach(function (element) {
+  		res.write('<li><a href=' + element.name + '.txt>' + element.name + '</a></li>');
+  	});
+  	res.write('</ul>');
+  	res.end();
+});
+
+setInterval(function() { 	
+	videosArray.forEach(function (element) {
+		
+		https.get("https://www.googleapis.com/youtube/v3/videos?id="+ element.id +"&key="+ API_KEY +"&part=statistics", 
+		function(res) {
+		  res.on("data", function(chunk) {
+		  	console.log("VideoName: " + element.name);
+		  	var item = JSON.parse(chunk);
+		  	var viewCount =  item.items[0].statistics.viewCount;
+		    console.log("ViewCount: " + viewCount);
+		    var likeCount = item.items[0].statistics.likeCount;
+		    console.log("likeCount: " + likeCount);
+		    var timeStamp = new Date().getTime()
+		    console.log("TimeStamp: " + timeStamp);
+
+		    fs.appendFile('data/' + element.name + '.txt', timeStamp + ':' + viewCount + ':' + likeCount + '//', function (err) {
+			  if (err) throw err;
+			  console.log('The "data to append" was appended to file!');
+			});
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		});
+	});	
+}, LAPSE);
